53e76536ec2b2d81f271ca633bb75966e0d41282
Yinyin L <yinyinl@gmail.com>
2024-02-17 03:25:33 +0800

    add helper function to make rule from CIDR

--- withinnet.go
+++ withinnet.go
@@ -6,6 +6,14 @@ import (
 
 type WithinNetworkRule net.IPNet
 
+func NewWithinNetworkRuleViaCIDR(s string) (*WithinNetworkRule, error) {
+	_, ipnet, err := net.ParseCIDR(s)
+	if err != nil {
+		return nil, err
+	}
+	return (*WithinNetworkRule)(ipnet), nil
+}
+
 func (rule *WithinNetworkRule) Contains(ip net.IP) bool {
 	return (*net.IPNet)(rule).Contains(ip)
 }
